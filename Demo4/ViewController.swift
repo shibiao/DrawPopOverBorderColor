7be6787bc6ddd7a2a2ffac25b57b33647c0cd9e2
shibiao <956035125@qq.com>
2017-01-12 19:50:12 +0800

    Initial Commit

---
+++ Demo4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demo4
+//
+//  Created by Mac on 2017/1/12.
+//  Copyright © 2017年 shibiao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
